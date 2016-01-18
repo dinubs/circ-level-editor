4d2ec0342b938fd320aba1d71df93c2fc38732e8
Gavin Dinubilo <gavin_dinubilo@mail.tmcc.edu>
2016-01-18 19:51:14 +0000

    fix issue with input not updating

--- app/assets/javascripts/components/level_form.js.jsx
+++ app/assets/javascripts/components/level_form.js.jsx
@@ -26,12 +26,15 @@ var LevelForm = React.createClass({
     this.refs.width.value = grid[0].length;
     return true;
   },
+  handleChange: function(e) {
+    this.setState({value: e.target.value});
+  },
   render: function() {
     return (
       <form className='' method='post' action={'/games/' + this.props.token} onSubmit={this.newLevel} ref='game_form'>
         <a href='/' className='logo'>Circ</a>
         <div className='right'>
-          <input type='text' name='game[name]' value={this.props.name} placeholder='Level Name' ref='game_name' />
+          <input type='text' name='game[name]' value={this.state.name} placeholder='Level Name' ref='game_name' onChange={this.handleChange} />
           <input type='hidden' name='game[map]' value={JSON.stringify(grid)} ref='map' />
           <input type='hidden' name='width' value={JSON.stringify(grid)} ref='width' />
           <input type='submit' value='Submit' />
