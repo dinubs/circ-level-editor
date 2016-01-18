6bfc4f9edaf283a6d09ab942b3ac2d9eee9a68ff
Gavin Dinubilo <gavin_dinubilo@mail.tmcc.edu>
2016-01-18 19:20:14 +0000

    change variable name

--- app/assets/javascripts/check_tile.js
+++ app/assets/javascripts/check_tile.js
@@ -5,7 +5,7 @@ var found_end = false;
 var checked = [];
 var time = 0;
 
-var tiles = [true, true, false, false, true, false, false];
+var tiles_to_check = [true, true, false, false, true, false, false];
 var end_index = 6;
 var prev_direction = '';
 var prev_direction_times = 0;
@@ -14,7 +14,7 @@ function check_tile(x, y) {
   var current_tile = map[y][x];
   console.log(x, y);
   if (current_tile === end_index) return true;
-  if (!tiles[current_tile]) return false;
+  if (!tiles_to_check[current_tile]) return false;
   if (checked.indexOf(x + '' + y) > -1) return false;
   checked.push(x + '' + y);
 
